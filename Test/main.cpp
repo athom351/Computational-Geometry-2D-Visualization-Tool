70312bc444feceebdc3397d43768ef19d8acc7bd
Code-Jp <epjpzheng@gmail.com>
2024-02-23 23:42:04 -0500

    Hotfix: update vcxproj

--- Test/main.cpp
+++ Test/main.cpp
@@ -86,79 +86,56 @@ typedef K::Segment_2 Segment_2;
 
 int main()
 {
-    Point_2 p0(12, -15), p1(-10, -25.3), p2(-0.01, 0.2);
+    //Point_2 p0(12, -15), p1(-10, -25.3), p2(-0.01, 0.2);
 
-    Point_2 s1(10, 10), s2(40, 40);
-    Segment_2 seg(s1, s2);
+    //Point_2 s1(10, 10), s2(40, 40);
+    //Segment_2 seg(s1, s2);
 
-    Point_2 p(10, 10), q(20, 15), s(10, 15);
-    Triangle_2 tri(p, q, s);
+    //Point_2 p(10, 10), q(20, 15), s(10, 15);
+    //Triangle_2 tri(p, q, s);
 
-    Circle_2 circ(p, 4);
+    //Circle_2 circ(p, 4);
 
-    Iso_rectangle_2 rect(p, q);
+    //Iso_rectangle_2 rect(p, q);
 
 
-    std::vector<std::string> geomObjectArray;
-    geomObjectArray.reserve(7); // only for observation purpose in this case; can be deleted
+    //std::vector<std::string> geomObjectArray;
+    //geomObjectArray.reserve(7); // only for observation purpose in this case; can be deleted
 
-    geomObjectArray.push_back(Geo2Util::toString(p0));
-    geomObjectArray.push_back(Geo2Util::toString(p1));
-    geomObjectArray.push_back(Geo2Util::toString(p2));
-    geomObjectArray.push_back(Geo2Util::toString(seg));
+    //geomObjectArray.push_back(Geo2Util::toString(p0));
+    //geomObjectArray.push_back(Geo2Util::toString(p1));
+    //geomObjectArray.push_back(Geo2Util::toString(p2));
+    //geomObjectArray.push_back(Geo2Util::toString(seg));
 
-    //geomObjectArray[0] = Geo2Util::toString(p0);
-    //geomObjectArray[1] = Geo2Util::toString(p1);
-    //geomObjectArray[2] = Geo2Util::toString(p2);
-    //geomObjectArray[3] = Geo2Util::toString(seg); 
-    //geomObjectArray[4] = Geo2Util::toString(s1);
-    //geomObjectArray[5] = Geo2Util::toString(s2);
+    ////geomObjectArray[0] = Geo2Util::toString(p0);
+    ////geomObjectArray[1] = Geo2Util::toString(p1);
+    ////geomObjectArray[2] = Geo2Util::toString(p2);
+    ////geomObjectArray[3] = Geo2Util::toString(seg); 
+    ////geomObjectArray[4] = Geo2Util::toString(s1);
+    ////geomObjectArray[5] = Geo2Util::toString(s2);
 
-    geomObjectArray.push_back(Geo2Util::toString(circ));
+    //geomObjectArray.push_back(Geo2Util::toString(circ));
 
-    //geomObjectArray[6] = Geo2Util::toString(circ);
-    //geomObjectArray[7] = Geo2Util::toString(p);
+    ////geomObjectArray[6] = Geo2Util::toString(circ);
+    ////geomObjectArray[7] = Geo2Util::toString(p);
 
-    geomObjectArray.push_back(Geo2Util::toString(tri));
+    //geomObjectArray.push_back(Geo2Util::toString(tri));
 
-    //geomObjectArray[8] = Geo2Util::toString(tri);
-    //geomObjectArray[9] = Geo2Util::toString(p);
-    //geomObjectArray[10] = Geo2Util::toString(q);
-    //geomObjectArray[11] = Geo2Util::toString(s);
+    ////geomObjectArray[8] = Geo2Util::toString(tri);
+    ////geomObjectArray[9] = Geo2Util::toString(p);
+    ////geomObjectArray[10] = Geo2Util::toString(q);
+    ////geomObjectArray[11] = Geo2Util::toString(s);
 
-    geomObjectArray.push_back(Geo2Util::toString(rect));
+    //geomObjectArray.push_back(Geo2Util::toString(rect));
 
-    //geomObjectArray[12] = Geo2Util::toString(rect);
-    //geomObjectArray[13] = Geo2Util::toString(p);
-    //geomObjectArray[14] = Geo2Util::toString(q);
+    ////geomObjectArray[12] = Geo2Util::toString(rect);
+    ////geomObjectArray[13] = Geo2Util::toString(p);
+    ////geomObjectArray[14] = Geo2Util::toString(q);
   
    
-    Geo2Util::printToFile("test.txt", geomObjectArray);
+    //Geo2Util::printToFile("test.txt", geomObjectArray);
+
+    Geo2Util::BoundaryType bt{ Geo2Util::BoundaryType::Solid };
+    cout << Geo2Util::toString(bt);
 
 }
-
-/*
-    ofstream file("test.txt");
-    
-    file << toString(p0) << endl << toString(p1) << endl << toString(p2) << endl << endl;
-
-    file.close();
-
-    cout << "Complete" << endl;
-
-    return 0;
-
-    */
-
-    /*
-    string toString(Point_2 p)
-    {
-        return "POINT " + to_string(p.x()) +  " " +
-            to_string(p.y()) + " 0 0 0 255 0 0 0 0 255";
-    }
-    */
-
-
-
-
-
